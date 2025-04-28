209c7b30703c7763a297214b1442970ab105af60
SatanaKILLER666 <mrtgolova@outlook.com>
2025-04-28 17:11:12 +0300

    Update bosinn.c

--- bosinn.c
+++ bosinn.c
@@ -1,105 +1,47 @@
-#include <iostream>
-#include <string>
-#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-class UIElement {
-public:
-    UIElement(const std::string& id, int x, int y) : id_(id), x_(x), y_(y) {}
-    virtual ~UIElement() {}
-
-    virtual void draw() = 0;
-    void setPosition(int x, int y) {
-        x_ = x;
-        y_ = y;
+// Функция для удаления символа из строки на месте (без доп. памяти)
+void remove_char(char *str, char char_to_remove) {
+  int i, j = 0;
+  for (i = 0; str[i] != '\0'; i++) {
+    if (str[i] != char_to_remove) {
+      str[j] = str[i];
+      j++;
     }
+  }
+  str[j] = '\0'; // Важно: Завершаем строку нулевым символом!
+}
 
-    const std::string& getId() const { return id_; }
-    int getX() const { return x_; }
-    int getY() const { return y_; }
+int main(int argc, char *argv[]) {
+  FILE *outfile;
 
-protected:
-    std::string id_;
-    int x_;
-    int y_;
-};
+  // Проверка количества аргументов командной строки
+  if (argc != 3) {
+    fprintf(stderr, "Использование: %s \"строка\" символ_для_удаления\n", argv[0]);
+    return 1;
+  }
 
-class Button : public UIElement {
-public:
-    Button(const std::string& id, int x, int y, const std::string& label)
-        : UIElement(id, x, y), label_(label) {}
+  // Получение аргументов командной строки
+  char *str = argv[1];
+  char char_to_remove = argv[2][0]; // Берем первый символ из второго аргумента
 
-    void draw() override {
-        std::cout << "Button: ID=" << id_ << ", Position=(" << x_ << ", " << y_
-                  << "), Label=" << label_ << std::endl;
-    }
+  // Удаление символа из строки
+  remove_char(str, char_to_remove);
 
-private:
-    std::string label_;
-};
+  // Открытие файла для записи
+  outfile = fopen("out.txt", "w");
+  if (outfile == NULL) {
+    perror("Ошибка при открытии файла out.txt");
+    return 1;
+  }
 
-class TextField : public UIElement {
-public:
-    TextField(const std::string& id, int x, int y, const std::string& text)
-        : UIElement(id, x, y), text_(text) {}
+  // Запись результата в файл
+  fprintf(outfile, "%s\n", str);
 
-    void draw() override {
-        std::cout << "TextField: ID=" << id_ << ", Position=(" << x_ << ", " << y_
-                  << "), Text=" << text_ << std::endl;
-    }
+  // Закрытие файла
+  fclose(outfile);
 
-private:
-    std::string text_;
-};
-
-class Label : public UIElement {
-public:
-    Label(const std::string& id, int x, int y, const std::string& text)
-        : UIElement(id, x, y), text_(text) {}
-
-    void draw() override {
-        std::cout << "Label: ID=" << id_ << ", Position=(" << x_ << ", " << y_
-                  << "), Text=" << text_ << std::endl;
-    }
-
-private:
-    std::string text_;
-};
-
-
-class GUI {
-public:
-    void addElement(UIElement* element) {
-        elements_.push_back(element);
-    }
-
-    void drawAll() {
-        for (UIElement* element : elements_) {
-            element->draw();
-        }
-    }
-
-    ~GUI() {
-        for (UIElement* element : elements_) {
-            delete element;
-        }
-    }
-
-private:
-    std::vector<UIElement*> elements_;
-};
-
-int main() {
-    GUI gui;
-
-    UIElement* button1 = new Button("button1", 10, 20, "Click Me!");
-    UIElement* textField1 = new TextField("textField1", 50, 100, "Enter text here");
-    UIElement* label1 = new Label("label1", 150, 50, "This is a label");
-
-    gui.addElement(button1);
-    gui.addElement(textField1);
-    gui.addElement(label1);
-
-    gui.drawAll();
-
-    return 0;
+  return 0;
 }
