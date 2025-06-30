9df639b169f56848b6c3e6a106aa63aa36da2b66
SatanaKILLER666 <mrtgolova@outlook.com>
2025-06-30 13:39:39 +0300

    Update bosinn.c

--- bosinn.c
+++ bosinn.c
@@ -1,94 +1,116 @@
-functions.h
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
+#ifndef COMPLEX_H
+#define COMPLEX_H
 
-#include <string>
-#include <vector>
+#include <iostream>
 
-template <typename T>
-int findIdx(const std::vector<T>& arr, const T& element);
+class Complex {
+private:
+  double real;
+  double imaginary;
 
-template <typename T>
-int count(const std::vector<T>& arr, const T& element);
+public:
+  Complex();
+  Complex(double real, double imaginary);
+  Complex(const Complex& other);
 
-template <typename T>
-std::string specialize(const T& element);
+  double getReal() const;
+  double getImaginary() const;
 
-template <>
-std::string specialize<bool>(const bool& element);
+  Complex operator+(const Complex& other) const;
+  Complex operator-(const Complex& other) const;
+  Complex operator*(const Complex& other) const;
+
+  Complex& operator+=(const Complex& other);
+  Complex& operator-=(const Complex& other);
+  Complex& operator*=(const Complex& other);
+
+  friend std::ostream& operator<<(std::ostream& os, const Complex& complex);
+};
 
 #endif
+#include "complex.h"
 
-functions.cpp
+Complex::Complex() : real(0.0), imaginary(0.0) {}
+
+Complex::Complex(double real, double imaginary) : real(real), imaginary(imaginary) {}
+
+Complex::Complex(const Complex& other) : real(other.real), imaginary(other.imaginary) {}
+
+double Complex::getReal() const { return real; }
+
+double Complex::getImaginary() const { return imaginary; }
 
 
-#include "functions.h"
+Complex Complex::operator+(const Complex& other) const {
+  return Complex(real + other.real, imaginary + other.imaginary);
+}
+
+Complex Complex::operator-(const Complex& other) const {
+  return Complex(real - other.real, imaginary - other.imaginary);
+}
+
+Complex Complex::operator*(const Complex& other) const {
+  double newReal = real * other.real - imaginary * other.imaginary;
+  double newImaginary = real * other.imaginary + imaginary * other.real;
+  return Complex(newReal, newImaginary);
+}
+
+Complex& Complex::operator+=(const Complex& other) {
+  real += other.real;
+  imaginary += other.imaginary;
+  return *this;
+}
+
+Complex& Complex::operator-=(const Complex& other) {
+  real -= other.real;
+  imaginary -= other.imaginary;
+  return *this;
+}
+
+Complex& Complex::operator*=(const Complex& other) {
+  double tempReal = real * other.real - imaginary * other.imaginary;
+  imaginary = real * other.imaginary + imaginary * other.real;
+  real = tempReal;
+  return *this;
+}
+
+std::ostream& operator<<(std::ostream& os, const Complex& complex) {
+  os << complex.real;
+  if (complex.imaginary >= 0) {
+    os << "+";
+  }
+  os << complex.imaginary << "i";
+  return os;
+}
 #include <iostream>
-#include <sstream>
-
-template <typename T>
-int findIdx(const std::vector<T>& arr, const T& element) {
-    for (size_t i = 0; i < arr.size(); ++i) {
-        if (arr[i] == element)
-            return static_cast<int>(i);
-    }
-    return -1;
-}
-
-template <typename T>
-int count(const std::vector<T>& arr, const T& element) {
-    int cnt = 0;
-    for (const auto& el : arr) {
-        if (el == element)
-            ++cnt;
-    }
-    return cnt;
-}
-
-template <typename T>
-std::string specialize(const T& element) {
-    std::ostringstream oss;
-    oss << element;
-    return oss.str();
-}
-
-template <>
-std::string specialize<bool>(const bool& element) {
-    return element ? "true" : "false";
-}
-
-template int findIdx<int>(const std::vector<int>&, const int&);
-template int findIdx<std::string>(const std::vector<std::string>&, const std::string&);
-template int findIdx<bool>(const std::vector<bool>&, const bool&);
-
-template int count<int>(const std::vector<int>&, const int&);
-template int count<std::string>(const std::vector<std::string>&, const std::string&);
-template int count<bool>(const std::vector<bool>&, const bool&);
-
-template std::string specialize<int>(const int&);
-template std::string specialize<std::string>(const std::string&);
-template std::string specialize<bool>(const bool&);
+#include "complex.h"
 
 int main() {
-    std::vector<int> vInt{1, 2, 3, 2, 4, 2};
-    std::vector<std::string> vStr{"apple", "banana", "apple", "cherry"};
-    std::vector<bool> vBool{true, false, true, true};
+  Complex c1;
+  Complex c2(2.0, 3.0);
+  Complex c3 = c2;
 
-    std::cout << "findIdx examples:" << std::endl;
-    std::cout << "Index of 2 in vInt: " << findIdx(vInt, 2) << std::endl;
-    std::cout << "Index of 'apple' in vStr: " << findIdx(vStr, std::string("apple")) << std::endl;
-    std::cout << "Index of false in vBool: " << findIdx(vBool, false) << std::endl;
+  std::cout << "c1: " << c1 << std::endl;
+  std::cout << "c2: " << c2 << std::endl;
+  std::cout << "c3: " << c3 << std::endl;
 
-    std::cout << "\ncount examples:" << std::endl;
-    std::cout << "Count of 2 in vInt: " << count(vInt, 2) << std::endl;
-    std::cout << "Count of 'apple' in vStr: " << count(vStr, std::string("apple")) << std::endl;
-    std::cout << "Count of true in vBool: " << count(vBool, true) << std::endl;
+  Complex c4 = c1 + c2;
+  std::cout << "c1 + c2: " << c4 << std::endl;
 
-    std::cout << "\nspecialize examples:" << std::endl;
-    std::cout << "specialize(123): " << specialize(123) << std::endl;
-    std::cout << "specialize(\"hello\"): " << specialize(std::string("hello")) << std::endl;
-    std::cout << "specialize(true): " << specialize(true) << std::endl;
-    std::cout << "specialize(false): " << specialize(false) << std::endl;
+  Complex c5 = c2 - c3;
+  std::cout << "c2 - c3: " << c5 << std::endl;
 
-    return 0;
+  Complex c6 = c2 * c3;
+  std::cout << "c2 * c3: " << c6 << std::endl;
+
+  c1 += c2;
+  std::cout << "c1 += c2: " << c1 << std::endl;
+
+  c1 -= c2;
+  std::cout << "c1 -= c2: " << c1 << std::endl;
+
+  c1 *= c2;
+  std::cout << "c1 *= c2: " << c1 << std::endl;
+
+  return 0;
 }
